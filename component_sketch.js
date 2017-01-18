04f21d70a3bde437ac98b07adf65276485374ea3
Fred Sladkey <fsladkey@gmail.com>
2017-01-18 18:04:24 -0500

    Clean up component sketch

--- component_sketch.js
+++ component_sketch.js
@@ -1,4 +1,3 @@
-// db("users").get.all({ where: { name: "name" }, order: "name", with: ["posts"] })
 // config object
 // {
 //   where: { col: "val" },
@@ -7,35 +6,6 @@
 //   page: { num, 1 perPage: 10 },
 //   limit: 5
 // }
-
-// FRONT END RESOURCE API
-// resources("posts").find(ownProps.params.id)
-// resources("posts").find({ title: "that one good post" });
-// resources("posts").all()
-// resources("posts").all({ where: { authorId: this.props.currentUser.id }});
-// resources("posts").read()
-
-
-// REQUEST/BACK END RESOURCE API
-// from("posts").get() // for singular item
-// from("posts").get(1)
-// from("posts").get({ session_token: "aosjn1234i120381y2"})
-// from("posts").get.all()
-// from("posts").get.all({ where: ["createdAt > ?", new Date()]})
-// from("posts").post({ title: "title" });
-// from("posts").patch({ id: 1, title: "title" });
-// from("posts").patch(1, { title: "title" });
-// from("posts").patch({ title: "old title"}, { title: "new title" });
-// from("posts").delete(1);
-// from("posts").delete();
-// from("posts").delete({ title: "delatable" });
-
-// FRONT END RESOURCE API
-// resources("posts").find(ownProps.params.id)
-// resources("posts").find({ title: "that one good post" });
-// resources("posts").all()
-// resources("posts").all({ where: { authorId: this.props.currentUser.id }});
-// resources("posts").read()
 
 import React, { Component } from 'react';
 import { connect } from 'core'
